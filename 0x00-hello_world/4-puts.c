9a399419296147006fc85e1a691713fde13d66fd
KALORSOR <alorsorgodsway@gmail.com>
2023-10-05 07:01:18 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,8 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ * main - "C program that prints  a sentence and ends on a new line"
+ * Return : 0
+ */
+int main( void)
+{puts ("\"Programming is like building a multilingual puzzle");
+return (0);}
