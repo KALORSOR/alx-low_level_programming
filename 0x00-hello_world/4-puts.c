ef33d8868b80ec8b2d073d77b41b15f2632fc7d8
KALORSOR <alorsorgodsway@gmail.com>
2023-10-05 08:07:14 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - "C program that prints  a sentence and ends on a new line"
- * Return : 0 (Good)
+ * main - Entry point
+ * Return: Always 0 (Good)
  */
 int main(void)
 {
-puts ("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
