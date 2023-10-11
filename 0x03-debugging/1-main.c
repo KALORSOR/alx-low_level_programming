b6ccb8be85a60f929a7e3747cfc0f84d195c82d8
KALORSOR <alorsorgodsway@gmail.com>
2023-10-11 08:01:03 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 
         printf("Infinite loop incoming :(\n");
 
-        i = 0; /*i is initialized to 0 just before the while loop*/
+        i = 0; /*i is initialized to 0 just before the while loop and not incremented in the while loop*/
 
         while (i < 10) /*while condition is given, an if the value of i is less than 10, the loop iterates*/
         {
