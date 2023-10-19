3ec1e5ac64cb79e17bac99f61f8e238975c42b1e
KALORSOR <alorsorgodsway@gmail.com>
2023-10-18 20:33:15 -0700

    6-cap-string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: returns length as integer;
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
+* cap_string - function that capitalize first character of a word
+* @str: string to capitalize
+* Return: returns the capitalized string
+*/
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[++i])
+	{
+		while (!(str[i] >= 'a') && (str[i] <= 'z'))
+			i++;
+
+		if (str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||
+				str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+				str[i - 1] == ';' ||
+				str[i - 1] == '.' ||
+				str[i - 1] == '!' ||
+				str[i - 1] == '?' ||
+				str[i - 1] == '"' ||
+				str[i - 1] == '(' ||
+				str[i - 1] == ')' ||
+				str[i - 1] == '{' ||
+				str[i - 1] == '}')
+			str[i] -= 32;
+	}
+
+	return (str);
+}
