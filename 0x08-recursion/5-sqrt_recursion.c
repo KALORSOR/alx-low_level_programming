8f41f13e6b40bd0de1814407806e2a696dcd2a33
KALORSOR <alorsorgodsway@gmail.com>
2023-10-25 18:59:45 -0700

    5-sqrt-recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * find_root - find square root of number, starting from the smallest possible, 0
+ * find_root - find square root of number, from the smallest possible, 0
  * @n: number
  * @root: test this root
  * Return: natural square root, or -1 if not natural root
