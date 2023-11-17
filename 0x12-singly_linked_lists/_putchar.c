6a35a55addf0897736e0d1e89be5e05d120cb843
KALORSOR <alorsorgodsway@gmail.com>
2023-11-16 17:35:39 -0800

    _putchar

--- 0x12-singly_linked_lists/_putchar.c
+++ 0x12-singly_linked_lists/_putchar.c
@@ -5,9 +5,11 @@
  * @c: The character to print
  *
  * Return: On success 1.
- * On error, -1 is returned, 
+ * On error, -1 is returned,
  */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+
