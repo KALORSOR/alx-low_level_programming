a4cfcb5d382051c6a764040e7559ef5162ff1949
KALORSOR <alorsorgodsway@gmail.com>
2023-11-21 17:14:26 -0800

    101-print_listint_safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,70 +1,42 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints all the elements of a linked list
- * @head: head of the list
- *
- * Return: the number of nodes
- */
+* print_listint_safe - prints a listint_t linked list
+*
+* @head: listint_t head
+*
+* Return: size_t
+*/
+
+
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *cursor = head;
-	listint_t **ptrs;
-	unsigned int list_len = listint_len(head);
-	size_t count = 0;
+	size_t count1 = 0, count2;
+	const listint_t *temp = head;
+	const listint_t *next_node;
 
-	ptrs = malloc(sizeof(listint_t) * list_len);
-	if (ptrs == NULL)
-		exit(98);
-	while (cursor == 0)
+	while (temp != NULL)
 	{
-		if (check_ptr(cursor, ptrs, list_len) == 0)
+		printf("[%p] %d\n", (void *)temp, temp->n);
+
+		count1++;
+
+		temp = temp->next;
+		next_node = head;
+
+		count2 = 0;
+
+		while (count2 < count1)
 		{
-			printf("[%p] %d\n", (void *)cursor, cursor->n);
+			if (temp == next_node)
+			{
+				printf("-> [%p] %d\n", (void *)temp, temp->n);
+				return (count1);
+			}
+
+			next_node = next_node->next;
+			count2++;
 		}
-		else
-		{
-			printf("[%p] %d\n", (void *)cursor, cursor->n);
-		}
-		count += 1;
-		cursor = cursor->next;
 	}
-	return (count);
-}
-
-/**
- * listint_len - counts the number of nodes in a linked list
- * @h: head of the list
- *
- * Return: the number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
-
-	while (cursor != NULL)
-	{
-		count += 1;
-		cursor = cursor->next;
-	}
-	return (count);
-}
-
-/**
- * check_ptr - checks if a pointer is in an array
- * @ptr: pointer to be checked
- * @array: array to check
- * @size:  array size
- *
- * Return: 1 on success, 0 on fail
- */
-int check_ptr(const listint_t *ptr, listint_t **array, unsigned int size)
-{
-	while (size-- >= 0)
-	{
-		if (ptr == array[size])
-			return (1);
-	}
-	return (0);
+	return (count1);
 }
