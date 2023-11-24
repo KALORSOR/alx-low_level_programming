f090808f5c21e0a0a555c0e9972154477b288dfb
KALORSOR <alorsorgodsway@gmail.com>
2023-11-23 17:10:38 -0800

    0-binary_to_unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,38 +1,29 @@
-#include "nain.h"
+#include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned integer
- * @b: string representing binary number
- * Return: converted number or 0 if b is NULL or if there are chars
- * that are not 0 or 1
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
+ *
+ * Return: converted number, or 0 if error
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len;
-	unsigned int i;
-	unsigned int j;
-	unsigned int sum;
-	unsigned int pow;
-	int base;
+	int i;
+	unsigned int num;
 
-	base = 2;
-	sum = 0;
-	pow = 1;
-	if (b == NULL)
+	num = 0;
+	if (!b)
 		return (0);
-	for (len = 0; b[len] != '\0'; len++) /*Getting the length of b num*/
-		;
-	if (len == 1 && (b[0] == '0' || b[0] == '1')) /*Checking single nums*/
-		return ((b[0] - 48));
-	for (i = 0; b[i] != '\0'; i++) /*Iterating through the string*/
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		for (j = len - 1; j > 0; j--) /*Calculating the exponent*/
-			pow = pow * base;
-		sum = sum + (pow * (b[i] - 48)); /*Adding the number*/
-		len--;
-		pow = 1; /*Reseting the exponent*/
 	}
-	return (sum);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
+	}
+	return (num);
 }
